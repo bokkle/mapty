b79bb7cc26af130a1c52ebe6f9cd343ef491cd3e
bokkle <mitch.r_@hotmail.com>
2024-01-06 17:06:00 -0500

    added proper rendering of cardio form

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // IMPLEMENT GEOLOGATION
 
 if (navigator.geolocation) {
@@ -25,31 +27,18 @@ if (navigator.geolocation) {
       // DISPLAY MAP USING LEAFLET
       // if using hosted version in html <head>, must link BEFORE script.js
 
-      const map = L.map('map').setView(coords, 12);
+      map = L.map('map').setView(coords, 12);
 
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup'
-            })
-          )
-          .setPopupContent('Cardio')
-          .openPopup();
+      //handling clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     () => {
@@ -57,3 +46,38 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //clear input fields
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  //display marker
+  console.log(mapEvent);
+
+  const { lat, lng } = mapEvent.latlng;
+
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Cardio')
+    .openPopup();
+});
+
+inputType.addEventListener('change', () => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
