02db33fb40f4fe55319d0708b9c58037b7632434
bokkle <mitch.r_@hotmail.com>
2024-01-05 23:55:05 -0500

    added geolocation

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// IMPLEMENT GEOLOGATION
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      // DISPLAY MAP USING LEAFLET
+      // if using hosted version in html <head>, must link BEFORE script.js
+
+      const map = L.map('map').setView(coords, 12);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    () => {
+      alert('Unable to locate your position');
+    }
+  );
+}
